5acc56d9928c22150c6450f7126137cfb9cab045
sudoBaymax <jatoujoseph@gmail.com>
2025-03-23 00:27:40 -0400

    working on the terminal instructions

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -17,7 +17,7 @@ function LandingPage() {
       <div className="CTA">
           {/* Call to Action */}
           <button className="mt-10 px-6 py-3 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700">
-            Get Started
+            &lt; Go to Shell &#47;&gt;
           </button>
 
           <a href="https://jatou.ca/resume/"></a><span className="mt-10 px-6 py-3 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700">
--- client/src/pages/TerminalPage.tsx
+++ client/src/pages/TerminalPage.tsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
 import '../styles/Terminal.css'
+import './terminalPage.css'
 import 'boxicons'
 import Terminal from '../components/Terminal.tsx'
 
 function TerminalPage() {
     return(
-        <Terminal />
+        <div className="terminal-page-wrapper">
+            <div className="terminal-functional">
+                <Terminal />
+            </div>
+            
+
+            <div className="terminal-explain">
+                <h1>Welcome to Shell</h1>
+
+                <h2>
+                If you’re not technical, that’s completely fine!
+
+                the window on your left is a terminal (glorified text editor), just type in “help” to see the available commands.
+
+                HINT: there is an easter egg command that does something really cool (first 15 people to find it get a starbucks giftcard for 15 Chinese Yen ¥)
+
+                You also get a giftcard if you give me a SWE internship :)
+
+                </h2>
+            </div>
+        </div>
     );
 }
 
